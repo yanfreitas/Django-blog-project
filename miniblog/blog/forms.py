2d98acb3de0905650a3cd3381134d18c9a02c469
Yan Freitas <yan_100@hotmail.com.br>
2020-08-20 15:40:25 -0300

    comments

--- miniblog/blog/forms.py
+++ miniblog/blog/forms.py
@@ -4,6 +4,13 @@ from blog.models import Comments
 
 class CreateComments(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(CreateComments, self).__init__(*args, **kwargs)  # Call to ModelForm constructor
+        self.fields['description'].widget.attrs['cols'] = 60
+        self.fields['description'].widget.attrs['rows'] = 10
+
     class Meta:
         model = Comments
-        fields = ('description', )
+        fields = ['description']
+        labels = {'description': ''}
+
