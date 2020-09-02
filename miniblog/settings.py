3456f83bb3dfcbdb3367b527789c5a0032553a53
Yan Freitas <yan_100@hotmail.com.br>
2020-09-02 19:01:47 -0300

    updated static root

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = 'blog/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
